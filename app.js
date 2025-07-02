b934d833952ede4a195f18b2a5c02f69d8bd9418
bhawnasingh89 <bhawnasingh8928@gmail.com>
2025-07-02 18:05:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let h1 = document.querySelector("h1");
+let container = document.querySelector(".container");
+let input = document.querySelector(".taskInput");
+let btn1 =  document.querySelector(".btn-add");
+let text = document.querySelector(".text");
+
+btn1.addEventListener("click",()=>{
+    if(input.value == ""){
+        alert("Please enter your task");
+    }else{
+        let newElement =  document.createElement("ul");
+        newElement.innerHTML = `${input.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newElement);
+        input.value = "";
+        newElement.querySelector("i").addEventListener("click",()=>{
+            newElement.remove();
+        })
+    }
+})
